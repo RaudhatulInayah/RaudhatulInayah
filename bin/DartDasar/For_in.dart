10d8fbc4265faa2594082963bc4fcddbeaddda02
RaudhatulInayah <raudhatulinayah5@gmail.com>
2024-10-19 13:10:08 +0800

    Update For_in.dart

--- bin/DartDasar/For_in.dart
+++ bin/DartDasar/For_in.dart
@@ -1,5 +1,4 @@
 void main() {
-  // Kode Tanpa for in (Menggunakan perulangan dengan indeks)
   var array = <String>['Raudhatul', 'Inayah']; 
   for (var i = 0; i < array.length; i++) {
     print('Tanpa for in: ${array[i]}');
@@ -7,7 +6,6 @@ void main() {
 
   print('Selesai perulangan tanpa for in.\n');
 
-  // Kode Menggunakan for in
   for (var value in array) {
     print('Dengan for in: $value');
   }
