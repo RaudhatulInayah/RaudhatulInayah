ca70595f1ec08cdd56f4c3ff1104ed1faf875ce9
RaudhatulInayah <raudhatulinayah5@gmail.com>
2024-10-19 13:11:49 +0800

    Update Named_Parameter.dart

--- bin/DartDasar/Named_Parameter.dart
+++ bin/DartDasar/Named_Parameter.dart
@@ -1,14 +1,11 @@
-// Function dengan Named Parameter
 void sayHello({String? firstName, String? lastName}) {
   print('Hello $firstName $lastName');
 }
 
-// Function dengan Default Parameter Value
 void sayHelloWithDefault({String? firstName, String lastName = 'Anonymous'}) {
   print('Hello $firstName $lastName');
 }
 
-// Function dengan Required Parameter
 void sayHelloRequired({required String firstName, String lastName = 'Default'}) {
   print('Hello $firstName $lastName');
 }
@@ -22,7 +19,6 @@ void main() {
 
   print('---');
 
-  // Contoh penggunaan Default Parameter Value
   sayHelloWithDefault(firstName: 'Raudhatul', lastName: 'Inayah');  
   sayHelloWithDefault(lastName: 'Hani', firstName: 'Ray'); 
   sayHelloWithDefault();  
@@ -30,7 +26,6 @@ void main() {
   sayHelloWithDefault(lastName: 'Hani');  
   print('---');
 
-  // Contoh penggunaan Required Parameter
   sayHelloRequired(firstName: 'Raudhatul', lastName: 'Inayah'); 
   sayHelloRequired(lastName: 'Hani', firstName: 'Ray');
   sayHelloRequired(firstName: 'Ray'); 
